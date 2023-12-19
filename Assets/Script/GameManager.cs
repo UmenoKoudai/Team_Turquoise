b1a50ef2a7c25aa83d8a1aff586e0d478aab4921
UmenoKoudai <umenokoudai0805@gmail.com>
2023-12-19 15:46:51 +0900

    [add]AudioController追加 [add]GameManager追加

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    Text __skillCountText;
+
+    [SerializeField]
+    GameState _state = GameState.Title;
+
+    private int _skillCount = 0;
+    public int SkillCount = 0;
+
+    public enum GameState
+    {
+        Title,
+        InGame,
+        GameOver,
+        Result,
+    }
+
+    void Update()
+    {
+        if (_state != GameState.InGame) return;
+        __skillCountText.text = _skillCount.ToString();
+    }
+
+    public void StateChange(GameState state)
+    {
+        _state = state;
+    }
+
+    public void SceneChange(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
