410e0a898e65fdf0cf86d9b7563a39e647ee0777
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-12-20 12:16:03 +0900

    [add]PlayerのActionImageを管理するクラス作成

---
+++ Assets/Script/Player/PlayerUIControlle.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>Player近くのにあるActionImageをコントロールする</summary>
+public class PlayerUIControlle : MonoBehaviour
+{
+    [SerializeField]
+    [Header("ActionImage表示用キャンバス")]
+    Canvas _actionCanvas;
+
+    [SerializeField]
+    [Header("ActionImage")]
+    Image _actionImage;
+
+    /// <summary>Playerオブジェクト原点としたCanvasのPositon</summary>
+    Vector2 _localPos;
+
+    private void Start()
+    {
+        //最初Player本体を親オブジェクトにする
+        _actionCanvas.transform.parent = transform.GetChild(0).GetComponent<Transform>();
+        _localPos = _actionCanvas.transform.localPosition;
+        ActionImageActive(false);
+    }
+    /// <summary>アクションのUIのImageを表示・非表示</summary>
+    /// <param name="active">表示・非表示</param>
+    public void ActionImageActive(bool active)
+    {
+        _actionImage.enabled = active;
+    }
+
+    public void PlayerBodyParentChange(Transform parent)
+    {
+        _localPos = _actionCanvas.transform.localPosition;
+        _actionCanvas.transform.parent = parent;
+        _actionCanvas.transform.localPosition = _localPos;
+    }
+}
